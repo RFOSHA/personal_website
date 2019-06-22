dea48566faf1bcb6fb5b21a303d83101cc0f832c
Ryan <ryan.m.foshage@gmail.com>
2019-06-22 15:33:52 -0500

    First commit

---
+++ freeze.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 15:13:47 2019
+
+@author: ryanm
+"""
+from flask_frozen import Freezer
+from myapp import app
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
